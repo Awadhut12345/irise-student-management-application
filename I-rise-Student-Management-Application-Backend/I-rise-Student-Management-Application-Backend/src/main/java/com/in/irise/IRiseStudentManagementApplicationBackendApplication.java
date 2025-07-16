145c213e0a427326ed402a693235fc9d03981ef8
nandkishorasambe98 <nandkishor.asambe@ashdipitsolutions.in>
2025-07-17 00:29:14 +0530

    add backend

---
+++ I-rise-Student-Management-Application-Backend/I-rise-Student-Management-Application-Backend/src/main/java/com/in/irise/IRiseStudentManagementApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.in.irise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IRiseStudentManagementApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IRiseStudentManagementApplicationBackendApplication.class, args);
+	}
+
+}
