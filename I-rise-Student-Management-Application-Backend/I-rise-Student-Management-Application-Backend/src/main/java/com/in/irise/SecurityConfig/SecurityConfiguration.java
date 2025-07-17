4e4c1bd0a856c35f2360d35dbc4e6a55520dc9de
nandkishorasambe98 <nandkishor.asambe@ashdipitsolutions.in>
2025-07-17 13:10:53 +0530

    add first page

---
+++ I-rise-Student-Management-Application-Backend/I-rise-Student-Management-Application-Backend/src/main/java/com/in/irise/SecurityConfig/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.in.irise.SecurityConfig;
+
+/**
+ *
+ * @author NandKishor
+ */
+public class SecurityConfiguration {
+    
+}
