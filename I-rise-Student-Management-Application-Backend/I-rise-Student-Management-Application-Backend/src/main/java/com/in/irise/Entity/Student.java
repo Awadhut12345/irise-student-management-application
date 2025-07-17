447982e0029d122f9d835ce26bd7bce0bb1016c8
Shankar Shirsat <123562202+Shankarshirsat896@users.noreply.github.com>
2025-07-17 17:16:48 +0530

    local changes

---
+++ I-rise-Student-Management-Application-Backend/I-rise-Student-Management-Application-Backend/src/main/java/com/in/irise/Entity/Student.java
@@ -0,0 +1,88 @@
+package com.in.irise.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private String address;
+	@Column(name = "email", unique = true)
+	private String email;
+	private long mobile_no;
+	private String password;
+	private String qualification;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobile_no() {
+		return mobile_no;
+	}
+	public void setMobile_no(long mobile_no) {
+		this.mobile_no = mobile_no;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", middleName=" + middleName + ", lastName="
+				+ lastName + ", address=" + address + ", email=" + email + ", mobile_no=" + mobile_no + ", password="
+				+ password + ", qualification=" + qualification + "]";
+	}
+
+}
