d2d03f79ae297f717598eac3531f1ecc3c58ab9c
nandkishorasambe98 <nandkishor.asambe@ashdipitsolutions.in>
2025-07-21 19:31:18 +0530

    add tailwind css

---
+++ I-rise-Student-Management-Application-Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import AdminDashboard from './Component/Admin/AdminDashboard'
+import LoginPage from './DashboardPage/LoginPage'
+
+function App() {
+ 
+
+  return (
+    <>
+        <AdminDashboard/>
+
+      <LoginPage/>
+    </>
+  )
+}
+
+export default App
---
+++ I-rise-Student-Management-Application-Frontend/src/DashboardPage/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react"
+import NavBar from "./NavBar";
+import GridLayout from 'react-grid-layout';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+
+const LoginPage = ()=>{
+
+    return (
+
+        <>
+        </>
+    )
+
+}
+export default  LoginPage;
